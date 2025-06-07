e8954e33b8fa1d1ab65f69f6cf0882065c5adc05
binbin <421099982@qq.com>
2025-06-07 15:11:48 +0800

    更新 package.json 以指定 pnpm 版本，修改 App.jsx 导入方式，注释掉 Navbar 组件内容并调整 Home.css 中标题颜色和字体大小以优化样式。

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-06-07 13:05:53
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-06-07 13:27:49
+ * @FilePath: /employment-service/src/App.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Policy from "./pages/Policy";
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,31 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <NavLink to="/" className="navbar-brand">
-          就业服务
-        </NavLink>
-        <div className="navbar-nav">
-          <NavLink to="/" className="nav-link" end>
-            首页
-          </NavLink>
-          <NavLink to="/policy" className="nav-link">
-            就业创业
-          </NavLink>
-          <NavLink to="/skill" className="nav-link">
-            技能培训
-          </NavLink>
-          <NavLink to="/jobs" className="nav-link">
-            招聘信息
-          </NavLink>
-          <NavLink to="/training" className="nav-link">
-            培训登记
-          </NavLink>
-        </div>
-      </div>
-    </nav>
-  );
+  return <></>
+  // return (
+  //   <nav className="navbar">
+  //     <div className="navbar-container">
+  //       <NavLink to="/" className="navbar-brand">
+  //         就业服务
+  //       </NavLink>
+  //       <div className="navbar-nav">
+  //         <NavLink to="/" className="nav-link" end>
+  //           首页
+  //         </NavLink>
+  //         <NavLink to="/policy" className="nav-link">
+  //           就业创业
+  //         </NavLink>
+  //         <NavLink to="/skill" className="nav-link">
+  //           技能培训
+  //         </NavLink>
+  //         <NavLink to="/jobs" className="nav-link">
+  //           招聘信息
+  //         </NavLink>
+  //         <NavLink to="/training" className="nav-link">
+  //           培训登记
+  //         </NavLink>
+  //       </div>
+  //     </div>
+  //   </nav>
+  // );
 }
